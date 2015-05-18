6164a07a4ab815c4fde527fb22f49302357ef78e
Vi Pham <tvpham11@gmail.com>
2015-05-18 14:56:06 -0400

    Added in JS and got time and background color to change! Yay!

---
+++ main.js
@@ -0,0 +1,27 @@
+var box = document.getElementById('box');
+var clock = document.getElementById('clock');
+
+function checkTime (x) {
+  if (x < 10) {
+    x = "0" + x;
+  }
+  return x;
+}
+
+function currentTime () {
+  var currentDay = new Date();
+  var hours = checkTime(currentDay.getHours());
+  var minutes = checkTime(currentDay.getMinutes());
+  var seconds = checkTime(currentDay.getSeconds());
+  var hexCode = '#' + hours + minutes + seconds;
+
+  clock.innerHTML = hexCode;
+  box.style.backgroundColor = hexCode;
+  clock.style.backgroundColor = hexCode;
+
+  var timer = setInterval(function () {
+            currentTime()
+        }, 1000);
+}
+
+currentTime();
